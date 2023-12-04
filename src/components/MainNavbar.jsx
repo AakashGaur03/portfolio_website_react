9a3ba16814fcf36b92b3f57e2c4bf7a43c61d7c5
AakashGaur03 <aakash.sunrise23@gmail.com>
2023-12-05 00:16:41 +0530

    Updated MainNavbar
    Updated scroll functionality
    was not working fine previously(Sometimes on Both Scroll and click 'active' class was active

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,37 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-scroll'
-import {useState} from 'react'
-import '../App.css'
+import '../App.css';
 
 const navItems = [
-    { id: 'home', label: 'Home' },
-    { id: 'about', label: 'About' },
-    { id: 'skills', label: 'Skills' },
-    { id: 'portfolio', label: 'Portfolio' },
-    { id: 'resume', label: 'Resume' },
-    { id: 'contact', label: 'Contact' },
-  ];
+  { id: 'home', label: 'Home' },
+  { id: 'about', label: 'About' },
+  { id: 'skills', label: 'Skills' },
+  { id: 'portfolio', label: 'Portfolio' },
+  { id: 'resume', label: 'Resume' },
+  { id: 'contact', label: 'Contact' },
+];
 
 function MainNavbar({onNavItemClick}) {
 
-    const [activeLink,setActiveLink]=useState('home')
-    const [activeLink2,setActiveLink2]=useState('')
+  const [activeLink,setActiveLink]=useState('home');
 
-    const handleNavItemClick=(link)=>{
-      setActiveLink2('');
-      setActiveLink(link);
-      onNavItemClick('');
-      onNavItemClick(link);
-    }
-    const handleSetActive = (to) => {
-      setActiveLink2(to);
-      setActiveLink('');
-      onNavItemClick('');
-      onNavItemClick(to);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
 
-    }
+      navItems.forEach(({ label, id }) => {
+        const section = document.getElementById(label);
+        if (!section) return;
+
+        const sectionTop = section.offsetTop - 10;
+        const sectionBottom = sectionTop + section.clientHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          setActiveLink(id);
+          onNavItemClick(id);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [onNavItemClick]);
+
+  const handleNavItemClick = (link) => {
+    setActiveLink(link);
+    onNavItemClick(link);
+  };
 
   return (
     <Navbar expand="lg" className="d-flex justify-content-center mainnavbar">
@@ -40,14 +54,15 @@ function MainNavbar({onNavItemClick}) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto navbaritems">
-            {navItems.map(({id,label})=>(
-                <Link to={label}  spy={true} 
-                smooth={true} 
-                offset={-10} 
-                duration={500}  key={id} onClick={()=>handleNavItemClick(id)} onSetActive={()=>handleSetActive(id)} className=
-                {`nav-link ${activeLink===id ? 'active' : ''} ${activeLink2===id ? 'active' : ''}`}>
-                    {label}
-                </Link>
+            {navItems.map(({id,label}) => (
+              <a
+                href={`#${label}`}
+                key={id}
+                onClick={() => handleNavItemClick(id)}
+                className={`nav-link ${activeLink===id ? 'active' : ''}`}
+              >
+                {label}
+              </a>
             ))}
           </Nav>
         </Navbar.Collapse>
