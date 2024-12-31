694ebc0120a60dda3a56bbcd86cd3b7863f0f03d
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-01-01 00:11:55 +0530

    Updated Year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-const Footer=()=>{
-    return(
-        <>
-        <div className="mainFooter">
-            <p className="footerHeading p-0 m-0">Aakash Gaur</p>
-            <p className="p-0 m-0">&copy; 2024 copyright all right reserved</p>
-        </div>
-        </>
-    )
-}
-export default Footer
+const Footer = () => {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+  return (
+    <>
+      <div className="mainFooter">
+        <p className="footerHeading p-0 m-0">Aakash Gaur</p>
+        <p className="p-0 m-0">&copy; {getCurrentYear()} copyright all right reserved</p>
+      </div>
+    </>
+  );
+};
+export default Footer;
